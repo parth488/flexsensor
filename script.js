1e9e385a0b29030c23978bc8629d3dc4405ef3a2
parth488 <67781271+parth488@users.noreply.github.com>
2022-09-03 23:21:20 +0530

    mmmm

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ function request_fullscreen() {
 }
 
 setInterval(function(){ 
-    document.getElementById("glow_circle").style.fill="#ff0000";
-    var speedval=Math.floor(Math.random() * (100 - 70 + 1)) + 70;
-    var tempval=Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+    // document.getElementById("glow_circle").style.fill="#ff0000";
+    var speedval= Number.parseFloat((Math.random() * (20 - 8 + 1)) + 8).toFixed(2);
+    // var tempval=Math.floor(Math.random() * (100 - 50 + 1)) + 50;
     document.getElementById("speed_guage").innerHTML = speedval;
-    document.getElementById("temp_display").innerHTML = tempval;
- if (speedval>=97){
-  document.getElementById("glow_circle").style.fill="#ff0000";
-  playSound()
- }
+    // document.getElementById("temp_display").innerHTML = tempval;
+    if(speedval>18){
+      playSound();
+      
+    }
 
- else{
-  document.getElementById("glow_circle").style.fill="#ffffff";
- }
-   }, 3000);
+   }, 4500);
   
   
    
-   
+   // Generate a random number between 2.5 and 10.75, including both 2.5 and 10.75, and specifying 2 decimal places
+// function generateRandomDecimalInRangeFormatted(min, max, places) {
+//   let value = (Math.random() * (max - min + 1)) + min;
+//   return Number.parseFloat((Math.random() * (20 - 8 + 1)) + 8).toFixed(2);
+// }
